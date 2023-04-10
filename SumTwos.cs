9892bd201d9202eb1893ca31d7289cbfddfefe93
Гниломёдова А.В <a.gnilomedova@centrofinans.ru>
2023-04-11 02:28:03 +0300

    Функция индексов элементов, дающих в сумме искомое число. Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

---
+++ SumTwos.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = { 2, 15, 7, 11 };
+int target = 9;
+int[] indices = TwoSum(arr, target);
+Console.WriteLine($"[{indices[0]}, {indices[1]}]");
+
+int[] TwoSum(int[] nums, int target)
+{
+    int[] ind = new int[2];
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                ind[0] = i;
+                ind[1] = j;
+                return ind;
+            }
+        }
+    }
+    return ind;
+}
