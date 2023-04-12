9c01dc6383a69c4489d761e0657021e8fe170ab2
Гниломёдова А.В <a.gnilomedova@centrofinans.ru>
2023-04-12 12:04:19 +0300

    Доработано определение номера котика

--- CatInTheHat.cs
+++ CatInTheHat.cs
@@ -1,29 +1,41 @@
 ﻿Console.WriteLine("Введите букву котика");
 
-string alphabet = "abcdefghijklmnopqrstuvwxyz";
+int catNumber;
+int minLatinCharCode = 97;
+int maxLatinCharCode = 122;
 string input;
+char cat;
+int catCode = 0;
+cat = (char)catCode;
+
 while (true)
 {
     input = Console.ReadLine();
     if (input == null)
     {
         continue;
-    }
+    };
     if (input.Equals("exit"))
     {
         return;
-    }
-    if (input.Length > 1 || input.Length <= 0 || alphabet.IndexOf(input.ToLower()) < 0)
+    };
+
+    if (input.Length == 1)
     {
-        Console.WriteLine("Введено неверное число, попробуйте ещё раз");
+        cat = input.ToLower()[0];
+        catCode = (int)cat;
+    };
+
+    if (input.Length > 1 || input.Length <= 0 || catCode < minLatinCharCode || catCode > maxLatinCharCode)
+    {
+        Console.WriteLine("Введена неверная буква котика, попробуйте ещё раз");
     }
     else
     {
         break;
     }
 }
-char cat = input.ToLower()[0];
-int catNumber = alphabet.IndexOf(cat) + 1;
+catNumber = catCode - minLatinCharCode + 1;
 double catInHatHeight = 2000000d;
 double totalHeight = totalCatHeight(catInHatHeight * 0.4, catNumber);
 Console.WriteLine($"Высота всех котиков {string.Format("{0:f3}", totalHeight)}, всего котиков {catNumber}");
