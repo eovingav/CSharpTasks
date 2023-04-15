f44bf3c9070f6531ee2f5c06be2f2e6992c2b982
Гниломёдова А.В <a.gnilomedova@centrofinans.ru>
2023-04-16 00:22:12 +0300

    Функция отделена пустой строкой

--- CatInTheHat.cs
+++ CatInTheHat.cs
@@ -35,6 +35,7 @@ while (true)
 double catInHatHeight = 2000000d;
 double totalHeight = totalCatHeight(catInHatHeight, catNumber + 1);
 Console.WriteLine($"Высота всех котиков {string.Format("{0:f3}", totalHeight)}, всего котиков {catNumber}");
+
 double totalCatHeight(double height, int catNumber)
 {
     int number = catNumber - 1;
